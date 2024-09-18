641de4969e8b50abcba73b37a64e0f533bd672aa
Matvey_Kh <Khudyakovmatvey2006@gmail.com>
2024-09-18 21:42:02 +0300

    1

--- homework1.java
+++ homework1.java
@@ -1,5 +1,10 @@
 class homework1 {
     public static void main(String[] args) {
+        int n = 5; 
+        while (n > 0){
+            int a = n % 2;
+            n = n / 2;
+        }
         System.out.println("Hello, World! 2");
     }
 }
