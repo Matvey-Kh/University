6076da9bd360d241d795a8e0560cf2805a5d95b7
Matvey_Kh <Khudyakovmatvey2006@gmail.com>
2024-09-18 23:54:22 +0300

    1.0.0

--- homework1.java
+++ homework1.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class homework1 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int n = scanner.nextInt();
+
         int a;
-        int n = 10; 
         String c = "";
         while (n > 0) {
             a = n % 2;
